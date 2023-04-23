02ef829ca50100edcb26439460198045a7f06e2c
Hai Nguyen <nguyenthanhhai200092@gmail.com>
2023-04-23 19:49:27 +0700

    add Header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <default-header></default-header>
     <Nuxt />
   </div>
 </template>
+
+<script>
+import DefaultHeader from '@/components/Header/DefaultHeader'
+export default {
+  components: {
+    DefaultHeader,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,5 @@
 <template>
-  <div class="root">
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <nuxt-link to="/">Home</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/decks">Decks</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/profile">Profile</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/settings">Settings</nuxt-link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </div>
+  <div class="root"></div>
 </template>
 
 <script>
