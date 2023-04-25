04423cdc610346c05cc22e370532c006d9fae893
hai.nguyen <hai.nguyen@gss-sol.com>
2023-04-25 15:32:04 +0700

    Add footer and banner section

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
     <default-header></default-header>
     <Nuxt />
+    <default-footer></default-footer>
   </div>
 </template>
 
 <script>
 import DefaultHeader from '@/components/Header/DefaultHeader'
+import DefaultFooter from '@/components/Footer/DefaultFooter'
 export default {
   components: {
     DefaultHeader,
+    DefaultFooter,
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="root"></div>
+  <div class="ct">
+    <div class="banner">
+      <div class="thumbnail-banner"></div>
+    </div>
+    <div class="popular-decks">
+      <h3 class="text-center my-3">3 Popular Decks For You</h3>
+      <div class="r">
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+        <div class="c_4">
+          <div class="card">Deck 1</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +25,16 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+.banner {
+  height: 220px;
+  margin-top: 2rem;
+  .thumbnail-banner {
+    background: url('https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/65/34/AU-T2-E-001-English-Display-Banner.jpg')
+      center center;
+    background-size: cover;
+    height: inherit;
+  }
+}
+</style>
