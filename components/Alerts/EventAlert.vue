d4de5b1c272f24e97ce12c630a95566239da8017
hai.nguyen <hainguyen@gmail.com>
2023-05-12 16:57:02 +0700

    add event alert use plugin

---
+++ components/Alerts/EventAlert.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="alert alert_success">This is alert</div>
+</template>
---
+++ plugins/core-component.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+import EventAlert from '@/components/Alerts/EventAlert'
+
+Vue.component('EventAlert', EventAlert);
