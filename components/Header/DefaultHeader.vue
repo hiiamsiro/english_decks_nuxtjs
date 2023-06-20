dc0c81d4c34229754f8ad813b72f2d7246bfee5a
hai.nguyen <hainguyen@gmail.com>
2023-06-20 18:18:28 +0700

    update landing page UI

--- components/Header/DefaultHeader.vue
+++ components/Header/DefaultHeader.vue
@@ -1,28 +1,159 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/decks">Decks</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/profile">Profile</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/settings">Settings</nuxt-link>
-        </li>
-      </ul>
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav
+      class="flex items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img
+            class="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          style="outline: none"
+          @click="handleToggleMenu('open')"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <nuxt-link class="text-sm font-semibold leading-6 text-gray-900" to="/"
+          >Home</nuxt-link
+        >
+        <nuxt-link
+          class="text-sm font-semibold leading-6 text-gray-900"
+          to="/decks"
+          >Decks</nuxt-link
+        >
+        <nuxt-link
+          class="text-sm font-semibold leading-6 text-gray-900"
+          to="/profile"
+          >Profile</nuxt-link
+        >
+        <nuxt-link
+          class="text-sm font-semibold leading-6 text-gray-900"
+          to="/settings"
+          >Settings</nuxt-link
+        >
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <nuxt-link
+          class="text-sm font-semibold leading-6 text-gray-900"
+          to="/login"
+          >Log in <span aria-hidden="true">&rarr;</span></nuxt-link
+        >
+      </div>
     </nav>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+    <div class="lg:hidden" role="dialog" aria-modal="true">
+      <!-- Background backdrop, show/hide based on slide-over state. -->
+      <div
+        ref="hamburgerMenu"
+        class="hamburger-menu fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img
+              class="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" style="outline: none" @click="handleToggleMenu('close')">
+            <span class="sr-only">Close menu</span>
+            <svg
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <nuxt-link
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                to="/"
+                >Home</nuxt-link
+              >
+              <nuxt-link
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                to="/decks"
+                >Decks</nuxt-link
+              >
+              <nuxt-link
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                to="/profile"
+                >Profile</nuxt-link
+              >
+              <nuxt-link
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                to="/settings"
+                >Settings</nuxt-link
+              >
+            </div>
+            <div class="py-6">
+              <nuxt-link
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                to="/login"
+                >Log in</nuxt-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
+<script>
+export default {
+  methods: {
+    handleToggleMenu(action) {
+      if (action === 'open') {
+        this.$refs.hamburgerMenu.style.transform = "translateY(0)"
+      } else if (action === 'close') {
+        this.$refs.hamburgerMenu.style.transform = "translateY(-100%)"
+      }
+    }
+  },
+}
+</script>
+
 <style lang="scss">
 header {
-  background-color: #000;
-  color: #fff;
   height: 60px;
   nav,
   ul {
@@ -49,5 +180,11 @@ header {
       }
     }
   }
+}
+.hamburger-menu {
+  transform: translateY(-100%);
+  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              opacity 0.55s ease;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,99 @@
 <template>
-  <div class="ct">
-    <!-- <event-alert class="mt-3" /> -->
-    <div class="banner">
-      <div class="thumbnail-banner"></div>
-    </div>
-    <div class="popular-decks">
-      <h3 class="text-center my-3">3 Popular Decks For You</h3>
-      <div class="r">
-        <div class="c_4">
-          <div class="card">Deck 1</div>
+  <div class="bg-white">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+      <div
+        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
+      </div>
+      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div class="text-center">
+          <h1>
+            <p
+              class="text-4xl font-bold tracking-tight mb-0 text-gray-900 sm:text-6xl text-gradient"
+              >Better way</p
+            >
+            <span
+              class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+              >to learn and memorize words</span
+            >
+          </h1>
+          <p class="mt-6 text-lg leading-10 text-gray-600">
+            <span class="flex justify-center">
+              <img src="@/assets/images/ic_checked.svg" alt="">
+              <span>AI-generated words made easy</span>
+            </span>
+            <span class="flex justify-center">
+              <img src="@/assets/images/ic_checked.svg" alt="">
+              <span>Get word insights with AI Assistant</span>
+            </span>
+            <span class="flex justify-center">
+              <img src="@/assets/images/ic_checked.svg" alt="">
+              <span>Gamify learning with word games</span>
+            </span>
+          </p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >Get started</a
+            >
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+              >Learn more <span aria-hidden="true">→</span></a
+            >
+          </div>
         </div>
-        <div class="c_4">
-          <div class="card">Deck 1</div>
-        </div>
-        <div class="c_4">
-          <div class="card">Deck 1</div>
-        </div>
+      </div>
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
       </div>
     </div>
   </div>
@@ -28,6 +106,9 @@ export default {
 </script>
 
 <style lang="scss">
+$tw-gradient-from: #71d16c;
+$tw-gradient-to: #0ea5e9;
+$tw-gradient-stops: $tw-gradient-from, $tw-gradient-to;
 .banner {
   height: 220px;
   margin-top: 2rem;
@@ -36,5 +117,11 @@ export default {
     background-size: cover;
     height: inherit;
   }
+}
+.text-gradient {
+  background-image: linear-gradient(to right, $tw-gradient-stops);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
 }
 </style>
